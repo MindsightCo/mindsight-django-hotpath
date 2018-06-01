e0eb3d56d5833be0aefeff7f06d721de3e43e53f
Eddy Reyes <ereyes01@users.noreply.github.com>
2018-06-01 01:43:39 -0500

    Fix dependencies to properly install pathlib for python <3.4

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
-import sys
 from setuptools import setup
 
-install_requires = ["requests", "django"]
-if sys.version_info[:2] < (3, 4):
-    install_requires.append('pathlib')
 
 setup(
     name="mindsight-django-hotpath",
-    version="0.3",
+    version="0.3.1",
     description="Mindsight Collector for Python/Django",
     url="https://github.com/MindsightCo/mindsight-django-hotpath",
     license="Apache 2.0",
     packages=["mindsight_django"],
-    install_requires=install_requires
+    install_requires=[
+        "requests",
+        "django",
+        'pathlib;python_version<"3.4"',
+    ]
 )
