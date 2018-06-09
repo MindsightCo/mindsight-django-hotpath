0f46699aa82fc06699be2ffab4426986772d8cad
Eddy Reyes <ereyes01@users.noreply.github.com>
2018-06-08 19:10:54 -0500

    Bump package version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="mindsight-django-hotpath",
-    version="0.3.1",
+    version="0.3.2",
     description="Mindsight Collector for Python/Django",
     url="https://github.com/MindsightCo/mindsight-django-hotpath",
     license="Apache 2.0",
